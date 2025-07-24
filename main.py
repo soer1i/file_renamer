dd966c01eeb4af0b6f83043bb47a49e518b49324
soerli <dimitri.gerster@gmail.com>
2025-07-24 12:39:30 +0200

    added replace feature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-def rename_files(target_folder, remove_first_n_chars : int, prefix : str, suffix : str):
+def rename_files(target_folder, remove_first_n_chars : int, prefix : str, suffix : str, replace):
     for filename in os.listdir(target_folder):
         extsep_index = os.path.basename(filename).index(os.path.extsep)
         new_filename = os.path.basename(filename)[:extsep_index]
@@ -11,8 +11,11 @@ def rename_files(target_folder, remove_first_n_chars : int, prefix : str, suffix
             new_filename = prefix + new_filename
         if suffix is not None:
             new_filename = new_filename + suffix
+        if replace is not None:
+            for replace_tuple in replace:
+                new_filename = new_filename.replace(replace_tuple[0], replace_tuple[1])        
         new_filename = new_filename + extension
         os.rename(os.path.join(target_folder, filename), os.path.join(target_folder, new_filename))
 
 if __name__ == "__main__":
-    rename_files('/Users/soerli/Downloads/für Dimi', 0, None, '_Janis')
+    rename_files('/Users/soerli/Downloads/Akira', 0, None, None, [['- c','- Chapter ']])
